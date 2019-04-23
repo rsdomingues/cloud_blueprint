8dce9054cf934e71946cabc5b3a30562860aed28
Rodrigo Domingues <rodrigosd@ciandt.com>
2019-04-23 15:05:48 -0700

    Configuring jenkins

---
+++ environment/accessJenkins.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+export POD_NAME=$(kubectl get pods -l "component=cd-jenkins-master" -o jsonpath="{.items[0].metadata.name}")
+kubectl port-forward $POD_NAME 8080:8080 >> /dev/null &
+
+export JENKINS_PASSWORD=$(printf $(kubectl get secret cd-jenkins -o jsonpath="{.data.jenkins-admin-password}" | base64 --decode);echo)
+echo "Use the password: $JENKINS_PASSWORD"
+
+open "http://localhost:8080"
